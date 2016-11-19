1b80ab0b7257946857c39784c6751b6b8e94fc11
Marko Hlebar <marko.hlebar@gmail.com>
2016-11-19 15:13:24 +0100

    Initial Commit

---
+++ CDTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CDTest
+//
+//  Created by Marko Hlebar on 19/11/2016.
+//  Copyright © 2016 Marko Hlebar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CDTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CDTest
+//
+//  Created by Marko Hlebar on 19/11/2016.
+//  Copyright © 2016 Marko Hlebar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
